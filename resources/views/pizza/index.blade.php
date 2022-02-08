4a2274f3f72b43c07347993263e46bebbd44e358
vermataruna <tarunaverma792@gmail.com>
2022-02-09 00:20:38 +0530

    add menu items on pizza listing screen

--- resources/views/pizza/index.blade.php
+++ resources/views/pizza/index.blade.php
@@ -3,6 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <div class="col-md-2">
+            <div class="card">
+                <div class="card-header">Menu</div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <a href="{{route('pizza.index')}}" class="list-group-item list-group-item-action">View</a>
+                        <a href="{{route('pizza.create')}}" class="list-group-item list-group-item-action">Create</a>
+                        <a href="{{route('pizza.order')}}" class="list-group-item list-group-item-action">User Order</a>
+                    </ul>
+                </div>
+            </div>
+        </div> 
         <div class="col-md-10">
             <div class="card">
                 <div class="card-header">All Pizza</div>
